2df8934c24a77cf5e9d5f98fdfe6da2aebfad904
LoungeTeam <141359379+onmydestiny@users.noreply.github.com>
2025-01-22 23:07:23 +0200

    Add files via upload

---
+++ Practical№6/main.py
@@ -0,0 +1,12 @@
+print("=== Анкета ===")
+
+name = input("Введіть ваше ім'я: ")
+age = input("Введіть ваш вік: ")
+city = input("Введіть ваше місто: ")
+hobby = input("Введіть ваше хобі: ")
+
+print("\n=== Ваша анкета ===")
+print(f"Ім'я: {name}")
+print(f"Вік: {age}")
+print(f"Місто: {city}")
+print(f"Хобі: {hobby}")
